f518c99bf51b0f4228b08a7201948f8309e85a9e
Filipe Sguarizi Panceri <filipe.info@gmail.com>
2015-09-24 02:50:49 +0000

    Add tasks controller/views

---
+++ app/views/tasks/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @task, :id, :name, :status, :project_id, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+   # Project resources
    resources :projects
+
+   # Tasks resources
+   resources :tasks
    root 'projects#index'
 end
